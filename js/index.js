c11aa8849afcc1e80ceeb4d1d676aca7b09143d8
Chris Corbin <cjcorbin@gmail.com>
2020-08-04 13:10:27 -0700

    mouseover, keydown

--- js/index.js
+++ js/index.js
@@ -4,14 +4,22 @@
 const introImg = document.querySelector('.intro img')
 
 introImg.addEventListener('mouseover', () => {
-    introImg.src = "https://vignette.wikia.nocookie.net/worldofcarsdrivein/images/a/a7/8_%282%29.jpg/revision/latest?cb=20180329101429"
+    introImg.src = 'https://vignette.wikia.nocookie.net/worldofcarsdrivein/images/a/a7/8_%282%29.jpg/revision/latest?cb=20180329101429'
+    introImg.style.height = 'auto'
+    introImg.style.width = '1000px'
     introImg.addEventListener('mouseout', () => {
-        introImg.src = "https://i.ytimg.com/vi/ZkBxl0vAqBo/maxresdefault.jpg";
+        introImg.src = 'https://i.ytimg.com/vi/ZkBxl0vAqBo/maxresdefault.jpg';
+        introImg.style.height = "auto";
+        introImg.style.width = "1000px";
     })
 })
 
 //   * `keydown`
+const newBg = document.querySelector('body')
 
+newBg.addEventListener('keydown' , () => {
+    newBg.style.backgroundColor='rebeccapurple';
+})
 
 //   * `wheel`
 
